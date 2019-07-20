e4f65e57ac4192d0224ef2a6a664b8da0aeee132
pvictor <pvictorcr@gmail.com>
2019-07-20 16:19:36 -0300

    Add PetType Services

---
+++ petclinic/pet-clinic-data/src/main/java/pvctr/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pvctr.springframework.petclinic.services;
+
+import pvctr.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ petclinic/pet-clinic-data/src/main/java/pvctr/springframework/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package pvctr.springframework.petclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import pvctr.springframework.petclinic.model.PetType;
+import pvctr.springframework.petclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public	Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+}
